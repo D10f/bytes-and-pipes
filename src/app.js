2a9a623c418815cf0008539fa2b3c17ac1576a12
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-02-13 15:43:27 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './styles.scss';
+
+// import { render } from 'react-dom';
+//
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hello World!</h1>
+//     </>
+//   );
+// };
+//
+// render(<App />, document.getElementById('root'));
