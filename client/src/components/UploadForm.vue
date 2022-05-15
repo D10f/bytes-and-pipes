8504cfa47fb968d5bfea8c2fa22cd38760355c83
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-05-15 02:29:55 +0200

    Started work on upload process

---
+++ client/src/components/UploadForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="options__container relative">
+    <p
+      v-if="!isReady"
+      class="text-gray-700 text-center text-sm mb-4 pointer-events-none"
+    >
+      Please make sure to complete all the steps to start the upload.
+    </p>
+    <button :disabled="!isReady" class="action-btn mx-auto">
+      Start Upload
+    </button>
+    <UploadFormOverlay v-if="progress" :progress="progress" />
+  </div>
+</template>
+
+<script>
+import UploadFormOverlay from './UploadFormOverlay.vue';
+
+export default {
+  name: 'UploadForm',
+  components: { UploadFormOverlay },
+  data() {
+    return {
+      progress: 0,
+    };
+  },
+  computed: {
+    isReady() {
+      return this.$store.getters.isReadyToUpload;
+    },
+  },
+  methods: {
+    startUpload() {
+      this.progress = 0;
+    },
+  },
+};
+</script>
---
+++ client/src/components/UploadFormOverlay.vue
@@ -0,0 +1,20 @@
+<template>
+  <aside
+    class="absolute top-0 left-0 w-full h-full bg-white/90 flex flex-col justify-center items-center text-back font-subtitle text-gray-700"
+  >
+    <p class="text-4xl">{{ progress }}%</p>
+    <p>Please don't close or refresh this window.</p>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'UploadFormOverlay',
+  props: {
+    progress: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
