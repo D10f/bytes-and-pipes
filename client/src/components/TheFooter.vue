a7ff7221948fbdcc386c0f1306fe17796016fec1
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-05-18 23:13:26 +0200

    Added router active link styles

--- client/src/components/TheFooter.vue
+++ client/src/components/TheFooter.vue
@@ -1,16 +1,16 @@
 <template>
   <footer class="border-t-2 border-primary-600 pt-2 sm:mt-10 lg:mt-0">
     <ul class="flex justify-between items-center lg:w-1/3 md:ml-auto">
-      <li class="footer__link">
+      <li class="footer__link" title="Link to GitHub.com.">
         <a href="https://github.com/D10f/bytes-and-pipes">Source Code</a>
       </li>
-      <li class="footer__link">
+      <li class="footer__link" title="Link to the about page.">
         <router-link :to="{ name: 'About' }">About</router-link>
       </li>
-      <li class="footer__link">
+      <li class="footer__link" title="Link to the privacy policy.">
         <router-link :to="{ name: 'Privacy' }">Privacy</router-link>
       </li>
-      <li class="footer__link">
+      <li class="footer__link" title="Link to the terms and conditions.">
         <router-link :to="{ name: 'Terms' }">Terms</router-link>
       </li>
     </ul>
