763fb42bff451993afb33bb5117fbd057615ae6a
D10f <galactodensimeter_bismuthyl@simplelogin.fr>
2022-09-10 19:23:10 +0200

    Major restructure to use Joi as validator for environment varibles. Add support for Docker Secrets. Add support for MAX_FILE_SIZE to be provided as a number of a string representing a storage unit eg 1GiB

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,27 +1,59 @@
+import { resolve } from 'path';
 import dotenv from 'dotenv';
+import * as Joi from 'joi';
+import { parseStorage, readDockerSecrets } from './utils';
+
+interface TSchema {
+  MONGODB_USERNAME: string;
+  MONGODB_PASSWORD: string;
+  MONGODB_DATABASE: string;
+  PUBLIC_DIR: string;
+  UPLOAD_LOCATION: string;
+  MAX_FILE_SIZE: number;
+  PORT: number;
+  DOMAIN: string;
+  LOG_LEVEL: string;
+  LOG_NAME: string;
+}
 
 dotenv.config();
 
-const config = {
-  //MONGODB SETTINGS
-  MONGODB_URI: process.env.MONGODB_URI,
-  MONGODB_USERNAME: process.env.MONGODB_USERNAME,
-  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
-  MONGODB_DATABASE: process.env.MONGODB_DATABASE,
+const envSchema = Joi.object({
+  MONGODB_USERNAME: Joi.string(),
+  MONGODB_USERNAME_FILE: Joi.string(),
+  MONGODB_PASSWORD: Joi.string(),
+  MONGODB_PASSWORD_FILE: Joi.string(),
+  MONGODB_DATABASE: Joi.string(),
+  MONGODB_DATABASE_FILE: Joi.string(),
 
-  //FILE UPLOAD/DOWNLOAD SETTINGS
-  MAX_FILE_SIZE: process.env.MAX_FILE_SIZE || 1024 * 1024 * 1024,
-  // PUBLIC_DIR: process.env.PUBLIC_DIR || resolve(__dirname, 'public'),
-  // UPLOAD_LOCATION: process.env.UPLOAD_LOCATION || resolve(__dirname, 'uploads'),
+  PUBLIC_DIR: Joi.string().default(resolve(__dirname, 'public')),
+  UPLOAD_LOCATION: Joi.string().default(resolve(__dirname, 'uploads')),
+  UPLOAD_LOCATION_FILE: Joi.string(),
 
-  //NODE.JS PROCESS
-  PORT: process.env.PORT || 3000,
-  HOST: process.env.HOST || '0.0.0.0',
+  MAX_FILE_SIZE: Joi.string().custom(parseStorage),
 
-  //APP SETTINGS
-  DOMAIN: process.env.DOMAIN || 'localhost',
-  LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
-  LOG_NAME: process.env.LOG_NAME || 'Bytes And Pipes [Development]',
-};
+  PORT: Joi.number().default(80),
 
-export default config;
+  DOMAIN: Joi.string().default('localhost'),
+  LOG_LEVEL: Joi.string().default('debug'),
+  LOG_NAME: Joi.string().default('Bytes And Pipes [Development]'),
+})
+  .xor('MONGODB_USERNAME_FILE', 'MONGODB_USERNAME')
+  .xor('MONGODB_PASSWORD_FILE', 'MONGODB_PASSWORD')
+  .xor('MONGODB_DATABASE_FILE', 'MONGODB_DATABASE')
+  .oxor('UPLOAD_LOCATION_FILE', 'UPLOAD_LOCATION')
+  .rename('MONGODB_USERNAME_FILE', 'MONGODB_USERNAME')
+  .rename('MONGODB_PASSWORD_FILE', 'MONGODB_PASSWORD')
+  .rename('MONGODB_DATABASE_FILE', 'MONGODB_DATABASE')
+  .rename('UPLOAD_LOCATION_FILE', 'UPLOAD_LOCATION');
+
+const config = envSchema.validate(readDockerSecrets(), {
+  allowUnknown: true,
+  stripUnknown: true,
+});
+
+if (config.error) {
+  throw new Error(config.error.details[0].message);
+}
+
+export default config.value as TSchema;
